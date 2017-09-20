c0818650235d2800268fa79ed3fdc1dce8b6ad7a
wz2793312458 <wz2793312458@163.com>
2017-09-20 15:48:58 +0800

    Initial commit

---
+++ app/src/main/java/com/taoshunda/mytime/MainActivity.java
@@ -0,0 +1,82 @@
+package com.taoshunda.mytime;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.jzxiang.pickerview.TimePickerDialog;
+import com.jzxiang.pickerview.data.Type;
+import com.jzxiang.pickerview.listener.OnDateSetListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements OnDateSetListener {
+    private TimePickerDialog mDialogMonthDayHourMinute;
+//    SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    SimpleDateFormat sf = new SimpleDateFormat("HH:mm");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn1:
+                mDialogMonthDayHourMinute = new TimePickerDialog.Builder()
+                        .setType(Type.ALL)
+                        .setCallBack(this)
+                        .setThemeColor(getResources().getColor(R.color.colorAccent))
+                        .build();
+                mDialogMonthDayHourMinute.show(getSupportFragmentManager(), "All");
+                break;
+            case R.id.btn2:
+                mDialogMonthDayHourMinute = new TimePickerDialog.Builder()
+                        .setType(Type.YEAR_MONTH_DAY)
+                        .setCallBack(this)
+                        .build();
+                mDialogMonthDayHourMinute.show(getSupportFragmentManager(), "YEAR_MONTH_DAY");
+                break;
+            case R.id.btn3:
+                mDialogMonthDayHourMinute = new TimePickerDialog.Builder()
+                        .setType(Type.HOURS_MINS)
+                        .setCallBack(this)
+                        .build();
+                mDialogMonthDayHourMinute.show(getSupportFragmentManager(), "HOURS_MINS");
+                break;
+            case R.id.btn4:
+                mDialogMonthDayHourMinute = new TimePickerDialog.Builder()
+                        .setType(Type.MONTH_DAY_HOUR_MIN)
+                        .setCallBack(this)
+                        .build();
+                mDialogMonthDayHourMinute.show(getSupportFragmentManager(), "month_day_hour_minute");
+                break;
+            case R.id.btn5:
+                mDialogMonthDayHourMinute = new TimePickerDialog.Builder()
+                        .setType(Type.YEAR_MONTH)
+                        .setCallBack(this)
+                        .build();
+                mDialogMonthDayHourMinute.show(getSupportFragmentManager(), "YEAR");
+                break;
+        }
+    }
+
+    @Override
+    public void onDateSet(TimePickerDialog timePickerView, long millseconds) {
+        String text = getDateToString(millseconds);
+        Toast.makeText(this, "" + text, Toast.LENGTH_SHORT).show();
+    }
+
+    public String getDateToString(long time) {
+        Date d = new Date(time);
+        return sf.format(d);
+    }
+}
